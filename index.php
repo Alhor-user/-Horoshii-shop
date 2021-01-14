ef995b07f23afaf51abcac44cf48dd4a90e8f6d0
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-01-14 13:07:03 +0700

    Первый коммит

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+
+</head>
+<body>
+    <nav class="uk-navbar-container" uk-navbar style="height: 160px;">
+        <div class="uk-navbar-center" style="height: 60px; margin-top: 80px;">
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a href="#">Продукция</a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li><a href="#">Морская рыба</a></li>
+                            <li><a href="#">Речная рыба</a></li>
+                            <li><a href="#">Замороженная рыба</a></li>
+                            <li><a href="#">Охлажденная рыба</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">Рецепты</a></li>
+                <li><a href="#">Наши магазины</a></li>
+                <li><a href="#">Контакты</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
