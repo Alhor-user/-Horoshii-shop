451b4ee691b1431fe0d82fe7a115d0bf63326615
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-01-15 21:21:23 +0700

    Доработка хедера, разбиение на блоки, сейв

---
+++ header.php
@@ -0,0 +1,37 @@
+<nav class="uk-navbar-container" uk-navbar style="height: 120px;">
+    
+    <div class="uk-flex">
+        <div>
+            <p>Logo</p>
+        </div>
+
+        <div class="uk-flex uk-flex-column">
+            <div>
+            </div>
+            
+            <div class="uk-navbar-center" style="height: 60px; margin-top: 80px;">
+                <ul class="uk-navbar-nav">
+                    <li>
+                        <a href="#">Продукция</a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="#">Морская рыба</a></li>
+                                <li><a href="#">Речная рыба</a></li>
+                                <li><a href="#">Замороженная рыба</a></li>
+                                <li><a href="#">Охлажденная рыба</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="#">Рецепты</a></li>
+                    <li><a href="#">Наши магазины</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div>
+            <p>Phone</p>
+        </div>
+    </div>
+
+</nav>
--- index.php
+++ index.php
@@ -1,28 +1,9 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <?php require_once("head.php") ?>
+    <?php require_once("head.php"); ?>
 </head>
 <body>
-    <nav class="uk-navbar-container" uk-navbar style="height: 160px;">
-        <div class="uk-navbar-center" style="height: 60px; margin-top: 80px;">
-            <ul class="uk-navbar-nav">
-                <li>
-                    <a href="#">Продукция</a>
-                    <div class="uk-navbar-dropdown">
-                        <ul class="uk-nav uk-navbar-dropdown-nav">
-                            <li><a href="#">Морская рыба</a></li>
-                            <li><a href="#">Речная рыба</a></li>
-                            <li><a href="#">Замороженная рыба</a></li>
-                            <li><a href="#">Охлажденная рыба</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li><a href="#">Рецепты</a></li>
-                <li><a href="#">Наши магазины</a></li>
-                <li><a href="#">Контакты</a></li>
-            </ul>
-        </div>
-    </nav>
+    <?php require_once("header.php"); ?>
 </body>
 </html>
