591a4a9342033812412a4f9ff5cf0a9f7bdf8cf4
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-02-09 19:06:21 +0700

    Добавил меню, установил размеры шрифтов и жирность

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<nav class="uk-navbar-container" uk-navbar style="height: 120px;">
+<nav class="uk-navbar-container" uk-navbar style="height: 150px;">
     
     <div uk-grid class="uk-width-1-1 uk-grid-collapse uk-child-width-expand">
     <div class="uk-width-1-4" style="background-color: red;">
@@ -8,11 +8,33 @@
 
     <div>
         <div uk-grid class="uk-width-1-1 uk-height-1-1 uk-grid-collapse uk-child-width-expand">
-            <div class="uk-width-1-1 uk-height-1-2" style="background-color: grey;">
+            <div class="uk-width-1-1" style="background-color: grey; height: 90px;">
                 <p>Logo</p>
             </div>
-            <div class="uk-width-1-1 uk-height-1-2" style="background-color: black;">
-                <p>Logo</p>
+            <div class="uk-width-1-1" style="background-color: black; height: 60px;">
+            
+            <!-- Начало элемента меню в центре -->
+                <div class="uk-navbar">
+                    <div class="uk-navbar-center">
+                        <ul class="uk-navbar-nav">
+                            <li>
+                                <a href="#" style="font-size: 1rem; font-weight: 500; min-height: 60px;">Продукция</a>
+                                <div class="uk-navbar-dropdown" style="box-shadow: none; background-color: #39f; padding:12px 25px 17px 25px;" uk-dropdown="offset: 0">
+                                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                                        <li><a href="#" style="font-size: 1rem;">Морская рыба</a></li>
+                                        <li><a href="#" style="font-size: 1rem;">Речная рыба</a></li>
+                                        <li><a href="#" style="font-size: .95rem;">Замороженная рыба</a></li>
+                                        <li><a href="#" style="font-size: .95rem;">Охлажденная рыба</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a href="#" style="font-size: 1rem; font-weight: 500; min-height: 60px;">Рецепты</a></li>
+                            <li><a href="#" style="font-size: 1rem; font-weight: 500; min-height: 60px;">Наши магазины</a></li>
+                            <li><a href="#" style="font-size: 1rem; font-weight: 500; min-height: 60px;">Контакты</a></li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
