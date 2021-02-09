898a4a835fced8f994beab7eddff859f08e28725
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-02-09 23:30:13 +0700

    Настроил фон шапки, затемнения вместо цветов при выделении

--- head.php
+++ head.php
@@ -3,5 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/uikit.min.css" />
 <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
 <script src="js/uikit.min.js"></script>
 <script src="js/uikit-icons.min.js"></script>
