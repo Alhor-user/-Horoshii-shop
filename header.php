9652d61dacb1947a1d2cab7fa783c8256de7aff8
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-02-09 19:54:15 +0700

    Настроил цвета при выделении пунктов меню, паддинги

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <nav class="uk-navbar-container" uk-navbar style="height: 150px;">
     
     <div uk-grid class="uk-width-1-1 uk-grid-collapse uk-child-width-expand">
-    <div class="uk-width-1-4" style="background-color: red;">
+    <div class="uk-width-1-4">
         <p>Logo</p>
     </div>
 
 
     <div>
         <div uk-grid class="uk-width-1-1 uk-height-1-1 uk-grid-collapse uk-child-width-expand">
-            <div class="uk-width-1-1" style="background-color: grey; height: 90px;">
+            <div class="uk-width-1-1" style="height: 90px;">
                 <p>Logo</p>
             </div>
-            <div class="uk-width-1-1" style="background-color: black; height: 60px;">
+            <div class="uk-width-1-1" style="height: 60px;">
             
             <!-- Начало элемента меню в центре -->
                 <div class="uk-navbar">
@@ -19,12 +19,12 @@
                         <ul class="uk-navbar-nav">
                             <li>
                                 <a href="#" style="font-size: 1rem; font-weight: 500; min-height: 60px;">Продукция</a>
-                                <div class="uk-navbar-dropdown" style="box-shadow: none; background-color: #39f; padding:12px 25px 17px 25px;" uk-dropdown="offset: 0">
+                                <div class="uk-navbar-dropdown" style="box-shadow: none; background-color: #0c85cc; padding: 0;" uk-dropdown="offset: 0; padding: 0;">
                                     <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li><a href="#" style="font-size: 1rem;">Морская рыба</a></li>
-                                        <li><a href="#" style="font-size: 1rem;">Речная рыба</a></li>
-                                        <li><a href="#" style="font-size: .95rem;">Замороженная рыба</a></li>
-                                        <li><a href="#" style="font-size: .95rem;">Охлажденная рыба</a></li>
+                                        <li><a href="#" style="font-size: 1rem; padding:15px 25px 7px 25px;">Морская рыба</a></li>
+                                        <li><a href="#" style="font-size: 1rem; padding:7px 25px 7px 25px;">Речная рыба</a></li>
+                                        <li><a href="#" style="font-size: .95rem; padding:7px 25px 7px 25px;">Замороженная рыба</a></li>
+                                        <li><a href="#" style="font-size: .95rem; padding:7px 25px 20px 25px;">Охлажденная рыба</a></li>
                                     </ul>
                                 </div>
                             </li>
@@ -39,7 +39,7 @@
         </div>
     </div>
 
-    <div class="uk-width-1-4" style="background-color: green;">
+    <div class="uk-width-1-4">
         <p>Logo</p>
     </div>
     
