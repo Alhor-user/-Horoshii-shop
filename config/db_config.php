8c3bfbe20ae34ad1ff2d846d70cdeabe69a92c3d
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-02-11 20:01:23 +0700

    Добавил подключение к БД

--- config/db_config.php
+++ config/db_config.php
@@ -1,22 +1,27 @@
 <?php
 
-    $link = "";
+    $link;
+    
+    // Функцию используем для того, чтобы спрятать данные для авторизации
     function db_connect () {
+
+        // Все переменные локальные, извне не узнать
         $db_address = "srv12.host-food.ru";
         $db_user = "h153523_test";
         $db_password = "123qwe!@#QWE";
         $db_dbname = "h153523_test";
         
-        // global $link;
+        // Указатель делаем глобальным, чтобы обращаться к БД извне
+        global $link;
         $link = mysqli_connect($db_address, $db_user, $db_password, $db_dbname);
 
+        // Проверка состояния подключения
         if ($link == false){
             print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
         }
-        else {
-            print("Соединение установлено успешно");
-        }
+        // else {
+        //     print("Соединение установлено успешно");
+        // }
     }
     db_connect ();
-    echo $link;
 ?>
