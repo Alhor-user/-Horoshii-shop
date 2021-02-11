3c162683712a621ca3f89dc6ccc6f6cd419b0c85
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-02-11 14:18:36 +0700

    overflow hidden не давала прокрутить страницу

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <?php require_once("head.php"); ?>
 </head>
-<body style="background-color: #f7f7f7; max-height: 800px; overflow: hidden;">
+<body style="background-color: #f7f7f7; max-height: 800px;">
     <?php require_once("header.php"); ?>
     <?php require_once("content.php"); ?>
     <?php require_once("footer.php"); ?>
