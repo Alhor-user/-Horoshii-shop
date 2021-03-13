5f2dc5f57088cbf2fc69a06c631876689756e95c
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-13 21:21:34 +0700

    Проверяю соединение с БД и выборку записей, бд немного заполнил

--- pages/category-content.php
+++ pages/category-content.php
@@ -1,3 +1,8 @@
 <?php
-    echo 'Привет, ' . $category . '!';
+    echo 'Привет, ' . $category . '! <br>';
+    $sql = "SELECT * FROM `production`";
+    $result = mysqli_query($link, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+        print("Название: " . $row['Name'] . "; Категория: . " . $row['Category'] . "<br>");
+    }
 ?>
