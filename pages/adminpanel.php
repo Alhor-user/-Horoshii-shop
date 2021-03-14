d64901deea8fc8630be59988546f82a1206438c4
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 22:02:28 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -20,7 +20,7 @@
                     <input class="uk-input" type="password" name="pass">
                 </div>
             </div>
-            <button class="uk-button uk-button-primary" type="submit" name="sumbit">Вход</button>
+            <button class="uk-button uk-button-primary" type="submit" name="sumbit" value="true">Вход</button>
         </form>
     </div>
 
