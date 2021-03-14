f427ede1188c6637ae54b6d04d60bea2f197faee
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 21:17:12 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -6,18 +6,13 @@
     
     <div class="uk-align-center">
         <form>
-            <div style="display: inline-block;">
-                <div class="uk-inline">
-                    <span class="uk-form-icon" uk-icon="icon: user"></span>
-                    <input class="uk-input" type="text">
-                </div>
+            <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                <input class="uk-input" type="text">
             </div>
-
-            <div class="uk-margin">
-                <div class="uk-inline">
-                    <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
-                    <input class="uk-input" type="text">
-                </div>
+            <div class="uk-inline">
+                <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                <input class="uk-input" type="text">
             </div>
         </form>
     </div>
