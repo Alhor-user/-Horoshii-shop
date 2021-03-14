e80a61e8adbaa0ef0d4e4777ec4492a193167498
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 22:24:15 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -1,4 +1,30 @@
 <?php require_once("../config/db_config.php"); ?>
+
+
+<?php 
+    //Проверяем, что юзер нажал на кнопку Вход
+    if ($_POST['sumbit'] == 'true') {
+        $passhash = hash('md5', hash('md5', $_POST['pass']));
+        //Ищем юзера с введенным логином и хэшем пароля
+        $sql = "SELECT * FROM `users` WHERE `Password`='" . $passhash . "' AND `Login`='" . $_POST['Login'] . "'";
+        $result = mysqli_query($link, $sql);
+        $row = mysqli_fetch_array($result);
+        //Если такого юзера нет - ошибка, иначе - открываем сессию и редиректим на главную
+        if ($row['Login'] <> '') {
+            session_start();
+        } else echo "ERROR";
+    }
+
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+    echo hash('md5', hash('md5', $_POST['pass']));
+?>
+
+
 <html lang="ru">
 <head>
     <?php require_once("head.php"); ?>
@@ -24,29 +50,6 @@
             <button class="uk-button uk-button-primary" type="submit" name="sumbit" value="true">Вход</button>
         </form>
     </div>
-
-    <?php 
-        //Проверяем, что юзер нажал на кнопку Вход
-        if ($_POST['sumbit'] == 'true') {
-            $passhash = hash('md5', hash('md5', $_POST['pass']));
-            //Ищем юзера с введенным логином и хэшем пароля
-            $sql = "SELECT * FROM `users` WHERE `Password`='" . $passhash . "' AND `Login`='" . $_POST['Login'] . "'";
-            $result = mysqli_query($link, $sql);
-            $row = mysqli_fetch_array($result);
-            //Если такого юзера нет - ошибка, иначе - открываем сессию и редиректим на главную
-            if ($row['Login'] <> '') {
-                echo $row['Login'];
-            } else echo "ERROR";
-        }
-
-        echo "<pre>";
-        print_r($_POST);
-        echo "</pre>";
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
-        echo hash('md5', hash('md5', $_POST['pass']));
-    ?>
 
 </body>
 </html> 
