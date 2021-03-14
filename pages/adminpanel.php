fd2549700e8a497b752b07a60673312f5308d79b
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 21:07:36 +0700

    Начало тестирования. Добавляю авторизацию админа, скрытая страница.

---
+++ pages/adminpanel.php
@@ -0,0 +1,28 @@
+<html lang="ru">
+<head>
+    <?php require_once("head.php"); ?>
+</head>
+<body style="background-color: #f7f7f7;">
+    
+    <form>
+        <div class="uk-margin">
+            <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                <input class="uk-input" type="text">
+            </div>
+        </div>
+
+        <div class="uk-margin">
+            <div class="uk-inline">
+                <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                <input class="uk-input" type="text">
+            </div>
+        </div>
+    </form>
+
+    <?php 
+
+    ?>
+
+</body>
+</html>
