f928d9b68549a4bbee149714904f6e76918b9eb4
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 21:32:23 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -4,8 +4,8 @@
 </head>
 <body style="background-color: #f7f7f7;">
     
-    <div class="uk-align-center"> <!-- Магическое число, 226 - это ширина полей ввода данных -->
-        <form style="width:226px; margin-top: 200px;">
+    <div class="uk-align-center" style="width:226px;"> <!-- Магическое число, 226 - это ширина полей ввода данных -->
+        <form style="padding-top: 200px;">
             <div class="uk-margin">
                 <div class="uk-inline">
                     <span class="uk-form-icon" uk-icon="icon: user"></span>
