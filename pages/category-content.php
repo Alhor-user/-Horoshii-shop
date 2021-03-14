b920f5625983001aec0d950d1426e8474aacf35a
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 13:56:34 +0700

    мини-тест

--- pages/category-content.php
+++ pages/category-content.php
@@ -13,29 +13,28 @@
 
     <div class="uk-width-1-1" style="margin: 0 10vw;">
         <div class="uk-grid-column-small uk-grid-row-small uk-text-center uk-flex-center" uk-grid style="margin: 0;">
-            <!-- карта №1 -->
+
 
             <?php
             while ($row = mysqli_fetch_array($result)) {
                 if ($row['Category'] == $category){
-                    print("Название: " . $row['Name'] . "; Категория: . " . $row['Category'] . "<br>");
+                    //print("Название: " . $row['Name'] . "; Категория: . " . $row['Category'] . "<br>");
+                    echo '<div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
+                        <a href="/catalog/ikra">
+                            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img src="../img/ikra.jpg" alt="" class="category-card uk-transition-scale-up uk-transition-opaque">
+                                    <div class="uk-overlay uk-overlay-primary uk-position-bottom category-card-text">
+                                        <p>Икра</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>'
                 }
             }; ?>
 
 
-
-            <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
-                <a href="/catalog/ikra">
-                    <div class="uk-card uk-card-default">
-                        <div class="uk-card-media-top">
-                            <img src="../img/ikra.jpg" alt="" class="category-card uk-transition-scale-up uk-transition-opaque">
-                            <div class="uk-overlay uk-overlay-primary uk-position-bottom category-card-text">
-                                <p>Икра</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
         </div>
     </div>
 
