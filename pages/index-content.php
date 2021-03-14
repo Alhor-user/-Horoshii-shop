d1c00b10e52321346ffcf06b15d38b6b6170aba4
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-14 16:35:24 +0700

    мини-тест

--- pages/index-content.php
+++ pages/index-content.php
@@ -5,7 +5,31 @@
 
     <div class="uk-width-1-1" style="margin: 0 10vw;">
         <div class="uk-grid-column-small uk-grid-row-small uk-text-center uk-flex-center" uk-grid style="margin: 0;">
-            <!-- карта №1 -->
+            
+            
+            
+            <?php
+                $sql = "SELECT * FROM `categories` WHERE `Status`='Active'";
+                $result = mysqli_query($link, $sql);
+                while ($row = mysqli_fetch_array($result)) {
+                    //print("Название: " . $row['Name'] . "; Категория: . " . $row['Category'] . "<br>");
+                    echo '<div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
+                        <a href="/catalog/', $row['Category'], '">
+                            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img src="../img/', $row['Image'], '" alt="" class="category-card uk-transition-scale-up uk-transition-opaque">
+                                    <div class="uk-overlay uk-overlay-primary uk-position-bottom category-card-text">
+                                        <a>', $row['Name'], '</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>';
+                }; 
+            ?>
+            
+            
+            <!-- карта №1
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <a href="/catalog/ikra">
                     <div class="uk-card uk-card-default">
@@ -18,7 +42,7 @@
                     </div>
                 </a>
             </div>
-            <!-- карта №2 -->
+            карта №2
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <a href="/catalog/river-fish">
                     <div class="uk-card uk-card-default">
@@ -31,7 +55,7 @@
                     </div>
                 </a>
             </div>
-            <!-- карта №3 -->
+            карта №3
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <a href="/catalog/sea-fish">
                     <div class="uk-card uk-card-default">
@@ -44,7 +68,7 @@
                     </div>
                 </a>
             </div>
-            <!-- карта №4 -->
+            карта №4
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <a href="/catalog/salt-fish">
                     <div class="uk-card uk-card-default">
@@ -57,7 +81,7 @@
                     </div>
                 </a>
             </div>
-            <!-- карта №5 -->
+            карта №5
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
@@ -68,7 +92,7 @@
                     </div>
                 </div>
             </div>
-            <!-- карта №6 -->
+            карта №6
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
@@ -79,7 +103,7 @@
                     </div>
                 </div>
             </div>
-            <!-- карта №7 -->
+            карта №7
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
@@ -90,7 +114,7 @@
                     </div>
                 </div>
             </div>
-            <!-- карта №8 -->
+            карта №8
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
@@ -101,7 +125,7 @@
                     </div>
                 </div>
             </div>
-            <!-- карта №9 -->
+            карта №9
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
@@ -112,7 +136,7 @@
                     </div>
                 </div>
             </div>
-            <!-- карта №10 -->
+            карта №10
             <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
@@ -122,11 +146,11 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 
     <div class="uk-width-1-1" style="height: 150px;">
     </div>
 
-</div>
+</div>           
