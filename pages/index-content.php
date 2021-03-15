7ccd79fb583a142c88159c3224a5c57d02bbb5d6
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 17:59:47 +0700

    мини-тест

--- pages/index-content.php
+++ pages/index-content.php
@@ -22,7 +22,7 @@
                             <a href="#modal-example-4" onclick="Edit($row[\'Category\'])")><i class="fa fa-trash fa-lg" style="color: black;"></i></a>
                         </div>
                         <div class="icon-edit" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 30px; z-index: 1; opacity: 0.8;">
-                            <a href="#" uk-modal);"><i class="fa fa-pencil fa-lg" style="color: black;"></i></a>
+                            <a href="#modal-example-4" uk-modal);"><i class="fa fa-pencil fa-lg" style="color: black;"></i></a>
                         </div>';
                         
 
