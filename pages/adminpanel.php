125be70a4ce59cb5fdda9d8acdb913e68be2ddca
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 11:55:48 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -4,5 +4,5 @@
   }
 ?>
 <div style="height: 30px; background-color: #39f; position: relative;">
-    <a style="color: white; margin-right: 10px; margin-top: 2px; position: absolute; right: 10px; font-size: 18px;" href='index.php?closesession=true'>Выйти</a>
+    <a style="color: white; margin-right: 10px; margin-top: 2px; position: absolute; right: 10px; font-size: 18px;" href='../pages/index.php?closesession=true'>Выйти</a>
 </div>
