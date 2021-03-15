b73ca0b309a94b3c597bef17d918f13ce2df59dc
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 11:38:34 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -1,3 +1,8 @@
+<?php 
+    function closesession() {
+        session_abort ();
+    }
+?>
 <div style="height: 30px; background-color: #39f; position: relative;">
-    <a style="color: white; margin-right: 10px; margin-top: 2px; position: absolute; right: 10px; font-size: 18px;">Выйти</a>
+    <a style="color: white; margin-right: 10px; margin-top: 2px; position: absolute; right: 10px; font-size: 18px;" onclick="closesession()">Выйти</a>
 </div>
