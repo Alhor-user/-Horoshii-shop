e56813b1cf8d4fc9f71bb63ba0f44e2c0a9fbafd
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 14:18:55 +0700

    мини-тест

--- pages/edit.php
+++ pages/edit.php
@@ -5,7 +5,7 @@
         
         if ($_POST['type']='content'){
             $string = mysqli_real_escape_string($link, $_POST['text']);
-            $sql = "UPDATE `content` SET `Content` = ". $string ." WHERE `ID`= ". $_POST['id'];
+            $sql = "UPDATE `content` SET `Content` = '". $string ."' WHERE `ID`= '". $_POST['id'] ."'";
             $result = mysqli_query($link, $sql);
 
             if ($result == false) {
