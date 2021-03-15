f9a7d4d1e43d63c9456a5e5ace845e107faaa1cc
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 22:26:24 +0700

    мини-тест

--- pages/header.php
+++ pages/header.php
@@ -151,8 +151,8 @@
                     <div class="uk-modal-dialog uk-modal-body">
                     <form enctype="multipart/form-data" id="delete-category" method="post" action="../pages/edit.php">
                     <legend class="uk-legend">Вы точно хотите удалить эту категорию?</legend>
-                    <img data-src="../img/ikra.jpg" width="300px" uk-img style="margin: 0 120px;">
-                    <input class="uk-margin-small-top" name="picture" type="file" accept="image/jpeg,image/png,image/gif" /> 
+                    <img class="uk-margin-medium-top" data-src="../img/ikra.jpg" width="300px" uk-img style="margin: 0 120px;">
+                    <input name="picture" type="file" accept="image/jpeg,image/png,image/gif" /> 
                     <input id="1233232" class="uk-input uk-margin-medium-top" style="text-align: center;" value="" disabled>
                     <input type="hidden" name="type" value="delete-category">
                     <p class="uk-text-center uk-margin-medium-top">
