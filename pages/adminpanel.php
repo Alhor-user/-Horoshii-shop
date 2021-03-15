151ea92bb21241fcfc673404df27114b15133e9d
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 12:05:11 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -1,8 +1,3 @@
-<?php
-  if (isset($_GET['closesession'])) {
-    session_abort();
-  }
-?>
 <div style="height: 30px; background-color: #39f; position: relative;">
-    <a style="color: white; margin-right: 10px; margin-top: 2px; position: absolute; right: 10px; font-size: 18px;" href='../pages/index.php?closesession=true'>Выйти</a>
+    <a style="color: white; margin-right: 10px; margin-top: 2px; position: absolute; right: 10px; font-size: 18px;" href='/logout.php'>Выйти</a>
 </div>
