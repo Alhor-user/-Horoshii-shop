25c4166e165a7bf2b8d347ada9e9c8f46dd6d5de
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 17:10:39 +0700

    мини-тест

--- pages/index-content.php
+++ pages/index-content.php
@@ -19,7 +19,7 @@
                     echo '<div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px; position: relative;">';
                     if (hash('md5', $_SESSION["login"]) == $_SESSION["key"]) {
                         echo '<div class="icon-delete" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 0px; z-index: 1; opacity: 0.8;">
-                            <a href="#modal-example-3" uk-toggle><i class="fa fa-trash fa-lg" style="color: black;"></i></a>
+                            <a href="#modal-example-4" uk-toggle><i class="fa fa-trash fa-lg" style="color: black;"></i></a>
                         </div>
                         <div class="icon-edit" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 30px; z-index: 1; opacity: 0.8;">
                             <a href="#" onclick="Edit("', $row['Category'], '");"><i class="fa fa-pencil fa-lg" style="color: black;"></i></a>
