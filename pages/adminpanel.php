3b913136d6d71b9fc84c81f23ee489b35ec90550
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 11:28:13 +0700

    мини-тест

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -1,3 +1,3 @@
 <div style="height: 30px; background-color: #39f; position: relative;">
-    <a style="color: white; margin-right: 10px; position: absolute; right: 10px;">Выйти</a>
+    <a style="color: white; margin-right: 10px; position: absolute; right: 10px; font-size: 18px;">Выйти</a>
 </div>
