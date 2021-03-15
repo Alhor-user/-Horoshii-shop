ea4cadabbe26877aade634ae6b319ef7454ce30e
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-15 23:44:17 +0700

    мини-тест

--- pages/edit.php
+++ pages/edit.php
@@ -25,17 +25,29 @@
         if ($_POST['type']=='edit-category'){
             if ( 0 < $_FILES['file']['error'] ) {
                 echo 'Error: ' . $_FILES['file']['error'] . '<br>';
+                $imgsrc = 'noimage.png';
             }
             else {
                 move_uploaded_file($_FILES['file']['tmp_name'], '../img/' . $_FILES['file']['name']);
+                $imgsrc = $_FILES['file']['name'];
             };
+            
 
-            // $sql = "DELETE FROM `categories` WHERE `Category`= '". $_POST['category'] ."'";
-            // $result = mysqli_query($link, $sql);
+            // Ищем категории с таким же названием 
+            $sql = "SELECT * FROM `categories` WHERE `Category`=\'". $_POST['category'] ."\'";
+            $result = mysqli_query($link, $sql);
 
-            // if ($result == false) {
-            //     print("Произошла ошибка при выполнении запроса");
-            // }
+            // Обновляем строку, если до этого не нашли таких же категорий
+            if (mysqli_num_rows($result)>0) {
+                $sql = "UPDATE `categories` SET `Category`=\'". $_POST['newcategory'] ."\' AND `Name`=\'". $_POST['newname'] ."\' AND `Image`=\'". $imgsrc ."\' WHERE `Category`=\'". $_POST['oldcategory'] ."\'";
+                $result = mysqli_query($link, $sql);
+
+                if ($result == false) {
+                    print("Произошла ошибка при выполнении запроса");
+                }
+            } else {
+                print("Произошла ошибка - категория с таким названием уже существует");
+            };
         };
         
         echo "<pre>";
