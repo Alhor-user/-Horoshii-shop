3230913e3a705b8007374f726edf93d035c58af1
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-16 00:07:16 +0700

    мини-тест

--- pages/edit.php
+++ pages/edit.php
@@ -34,12 +34,12 @@
             
 
             // Ищем категории с таким же названием 
-            $sql = "SELECT * FROM `categories` WHERE `Category`='". $_POST['oldcategory'] ."'";
+            $sql = "SELECT * FROM `categories` WHERE `Category`='". $_POST['newcategory'] ."'";
             $result = mysqli_query($link, $sql);
             echo mysqli_num_rows($result);
 
             // Обновляем строку, если до этого не нашли таких же категорий
-            if (mysqli_num_rows($result)>0) {
+            if (mysqli_num_rows($result) == 0) {
                 $sql = "UPDATE `categories` SET `Category`='". $_POST['newcategory'] ."', `Name`='". $_POST['newname'] ."', `Image`='". $imgsrc ."' WHERE `Category`='". $_POST['oldcategory'] ."'";
                 echo $sql;
                 $result = mysqli_query($link, $sql);
