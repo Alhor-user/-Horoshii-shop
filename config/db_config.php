71e590c273604498bad6e62ae63cff3433cf40a9
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-17 20:45:01 +0700

    мини-тест

--- config/db_config.php
+++ config/db_config.php
@@ -17,7 +17,7 @@
 
         // Проверка состояния подключения
         if ($link == false){
-            print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+            //print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
         }
         // else {
         //     print("Соединение установлено успешно");
