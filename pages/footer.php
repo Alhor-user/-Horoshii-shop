820fa9af0a55fba997b885323e4cd5e28352885e
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-17 22:18:44 +0700

    мини-тест

--- pages/footer.php
+++ pages/footer.php
@@ -58,13 +58,13 @@ function DeleteCategory(category, name) {
 
 <script>
 function EditCategory(category, name) {
-    document.getElementById('formcategory').reset();
     console.log('1');
     console.log(category);
     console.log(name);
     document.getElementById('editCategoryCategory').defaultValue = "";
     document.getElementById('editCategoryOldCategory').defaultValue = "";
     document.getElementById('editCategoryName').defaultValue = "";
+    document.getElementById('formcategory').reset();
     document.getElementById('editCategoryCategory').setAttribute('value', category);
     document.getElementById('editCategoryOldCategory').setAttribute('value', category);
     document.getElementById('editCategoryName').setAttribute('value', name);
