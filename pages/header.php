8b6e240096ab97ab3cbce7cdbd50cf3180be155d
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-17 22:19:53 +0700

    мини-тест

--- pages/header.php
+++ pages/header.php
@@ -197,13 +197,13 @@
 
 
                 <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
-                <input id="editCategoryName" class="uk-input" name="newname" value="" required>
+                <input id="newCategoryName" class="uk-input" name="newname" value="" required>
                 <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Категория отображается в адресной строке и должна быть написана на английском, например \'ikra\'"></i> Название категории:</p>
-                <input id="editCategoryCategory" class="uk-input" name="newcategory" value="" required>
+                <input id="newCategoryCategory" class="uk-input" name="newcategory" value="" required>
                 <input type="hidden" name="type" value="edit-category">
                 <p class="uk-text-center uk-margin-medium-top">
                 <button class="uk-button uk-button-danger uk-modal-close" type="button">Отмена</button>
-                <button id="editCategoryOldCategory" class="uk-button uk-button-primary" type="submit" name="oldcategory" value="">Сохранить</button>
+                <button id="newCategoryOldCategory" class="uk-button uk-button-primary" type="submit" name="oldcategory" value="">Сохранить</button>
                 </p>
                 </form>
                 </div> 
