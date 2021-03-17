d98a70f15c645b59d6aaab5003a649f358255ff2
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-17 23:26:17 +0700

    мини-тест

--- pages/category-content.php
+++ pages/category-content.php
@@ -17,7 +17,18 @@
 
             <?php
             while ($row = mysqli_fetch_array($result)) {
-                //print("Название: " . $row['Name'] . "; Категория: . " . $row['Category'] . "<br>");
+                
+                if (hash('md5', $_SESSION["login"]) == $_SESSION["key"]) {
+                    echo '<div class="icon-delete" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 0px; z-index: 1; opacity: 0.8;">
+                        <a href="#modal-example-7" uk-toggle onclick="DeleteItem(\'', $row['ID'], '\', \'', $row['Name'], '\')"><i class="fa fa-trash fa-lg" style="color: black;"></i></a>
+                    </div>
+                    <div class="icon-edit" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 30px; z-index: 1; opacity: 0.8;">
+                        <a href="#modal-example-8" uk-toggle onclick="EditItem(\'', $row['ID'], '\', \'', $row['Name'], '\')"><i class="fa fa-pencil fa-lg" style="color: black;"></i></a>
+                    </div>';
+
+                }
+                
+                
                 echo '<div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">
                     <a>
                         <div class="uk-card uk-card-default" style="box-shadow: none;">
