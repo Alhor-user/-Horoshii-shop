c37829dbfe087b94b9a58e83b35e0b9b4c30e81e
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 22:09:13 +0700

    Добавляю заглушку, когда товара нет

--- pages/category-content.php
+++ pages/category-content.php
@@ -20,15 +20,16 @@
                 echo '<div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">';
                 
                 if (hash('md5', $_SESSION["login"]) == $_SESSION["key"]) {
-                    echo '<div class="icon-delete" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 0px; z-index: 1; opacity: 0.8;">
+                    echo '
+                    <div class="icon-delete" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 0px; z-index: 1; opacity: 0.8;">
                         <a href="#modal-example-7" uk-toggle onclick="DeleteItem(\'', $row['ID'], '\', \'', $row['Name'], '\')"><i class="fa fa-trash fa-lg" style="color: black;"></i></a>
-
-                        </div>
+                    </div>
                     <div class="icon-edit" style="position: absolute; width: 30px; height: 30px; background-color: white; top: 0px; right: 30px; z-index: 1; opacity: 0.8;">
                         <a href="#modal-example-8" uk-toggle onclick="EditItem(\'', $row['Name'], '\',\'', $row['Description'], '\',\'', $row['Price'], '\',\'', $row['Count'], '\',\'', $category, '\',\'', $row['Is-new'], '\',\'', $row['Is-no-stock'], '\',\'', $row['ID'], '\')"><i class="fa fa-pencil fa-lg" style="color: black;"></i></a>
                     </div>';
 
                 }
+
                 
                 
                 
@@ -42,7 +43,9 @@
                                 <p class="uk-margin-small-top">', $row['Description'], '</p>
                             </div>
                         </div>
-                    </a>';
+                    </a>
+                </div>';
+
             }; 
             
             if (hash('md5', $_SESSION["login"]) == $_SESSION["key"]) {
