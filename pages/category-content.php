fce2697f43031a1022716a7f5b9cdec667810a02
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 23:54:39 +0700

    Добавляю пагинацию

--- pages/category-content.php
+++ pages/category-content.php
@@ -1,55 +1,5 @@
 <?php
-    function pagePrint($page, $title, $show, $active_class = '') {
-        if($show) {
-            echo '<a href="?do=list&page=' . $page . '">' . $title . '</a>';
-        } else {
-            if(!empty($active_class)) $active = 'class="' . $active_class . '"';
-            echo '<span ' . $active . '>' . $title . '</span>';
-        }
-        return false;
-    }
-
-
-    $page_setting = [
-        'limit' => 8, // кол-во записей на странице
-        'show'  => 2, // 5 до текущей и после
-        'prev_show' => 0, // не показывать кнопку "предыдущая"
-        'next_show' => 0, // не показывать кнопку "следующая"
-        'first_show' => 0, // не показывать ссылку на первую страницу
-        'last_show' => 0, // не показывать ссылку на последнюю страницу
-        'prev_text' => 'назад',
-        'next_text' => 'вперед',
-        'class_active' => 'active',
-        'separator' => ' ... ',
-    ];
-    $start = ($page-1)*$page_setting['limit'];
-
-
-    $res = $link->query("SELECT count(*) AS count FROM `production` WHERE `Category`='" . mysqli_real_escape_string($link, $category) . "'");
-    $row = $res->fetch(PDO::FETCH_ASSOC);
-    // $sql = ;
-    // $res = mysqli_query($link, $sql);
-    // $row = $res->fetch(PDO::FETCH_ASSOC);
-    $page_count = ceil($row['count'] / $page_setting['limit']); // кол-во страниц
-    $page_left = $page - $page_setting['show']; // находим левую границу
-    $page_right = $page + $page_setting['show']; // находим правую границу
-    $page_prev = $page - 1; // узнаем номер предыдушей страницы
-    $page_next = $page + 1; // узнаем номер следующей страницы
-    if($page_left < 2) $page_left = 2; // левая граница не может быть меньше 2, так как 2 - первое целое число после 1
-    if($page_right > ($page_count - 1)) $page_right = $page_count - 1; // правая граница не может ровняться или быть больше, чем всего страниц
-    if($page > 1) $page_setting['prev_show'] = 1; // если текущая страница не первая, значит существует предыдущая
-    if($page != 1) $page_setting['first_show'] = 1; // показываем ссылку на первую страницу, если мы не на ней
-    if($page < $page_count) $page_setting['next_show'] = 1; // если текущая страница не последняя, значит существуюет следующая
-    if($page != $page_count) $page_setting['last_show'] = 1;
-
-
-    $page = (int) $_GET['page'];
-    if(empty($page)) $page = 1; // если страница не задана, показываем первую
-
-
-    
-
-    $sql = "SELECT * FROM `production` WHERE `Category`='" . mysqli_real_escape_string($link, $category) . "' ORDER BY `Name` LIMIT {$start},{$page_setting['limit']}";
+    $sql = "SELECT * FROM `production` WHERE `Category`='" . mysqli_real_escape_string($link, $category) . "' ORDER BY `Name` LIMIT $offset, $size_page";
     $result = mysqli_query($link, $sql);
 ?>
 
@@ -120,17 +70,4 @@
     <div class="uk-width-1-1" style="height: 50px;">
     </div>
 
-    <?php
-        pagePrint($page_prev, $page_setting['prev_text'], $page_setting['prev_show']);
-        pagePrint(1, 1, $page_setting['first_show'], $page_setting['class_active']);
-        if($page_left > 2) echo $page_setting['separator'];
-        for($i = $page_left; $i <= $page_right; $i++) {
-            $page_show = 1;
-            if($page == $i) $page_show = 0;
-            pagePrint($i, $i, $page_show, $page_setting['class_active']);
-        }
-        if($page_right < ($page_count - 1)) echo $page_setting['separator'];
-        if($page_count != 1) pagePrint($page_count, $page_count, $page_setting['last_show'], $page_setting['class_active']);
-        pagePrint($page_next, $page_setting['next_text'], $page_setting['next_show']);
-    ?>
 </div>
