05df4c62abbe5f6a58b487968fb81f725411d1eb
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 14:08:14 +0700

    Добавляю возможность редактирования товара

--- pages/modal-windows.php
+++ pages/modal-windows.php
@@ -125,5 +125,160 @@ echo'
     </div> 
 </div>';
 
+// Модальное окно для добавления новой категории
+echo'
+<div id="modal-example-6" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+        <form enctype="multipart/form-data" id="newcategoryform" method="post" action="../pages/new.php">
+            <legend class="uk-legend">Добавление новой категории</legend>
 
+            <div class="container uk-margin-medium-top">
+                <div class="row">
+                    <label><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Если не выбрать файл, то будет установлена картинка по-умолчанию"></i> Картинка категории (будет сжата до квадратной):</label><br>
+                    <input class="uk-margin-small-top" type="file" id="file" name="file" />
+                </div>
+                <div class="row">
+                    <span class="uk-margin-small-top" id="output"></span>
+                </div>
+            </div>
+
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
+            <input id="newCategoryName" class="uk-input" name="newname" value="" required>
+            
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Категория отображается в адресной строке и должна быть написана на английском, например \'ikra\'"></i> Название категории:</p>
+            <input id="newCategoryCategory" class="uk-input" name="newcategory" value="" required>
+            
+            <input type="hidden" name="type" value="new-category">
+            
+            <p class="uk-text-center uk-margin-medium-top">
+                <button class="uk-button uk-button-danger uk-modal-close" type="button">Отмена</button>
+                <button id="newCategoryOldCategory" class="uk-button uk-button-primary" type="submit" name="oldcategory" value="">Сохранить</button>
+            </p>
+        </form>
+    </div> 
+</div>';
+
+
+// Модальное окно для удаления товара
+echo'
+<div id="modal-example-7" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+        <form id="deleteitemform" method="post" action="../pages/delete.php">
+            <legend class="uk-legend">Вы точно хотите удалить этот товар?</legend>
+            
+            <input id="deleteItemName" class="uk-input uk-margin-medium-top" style="text-align: center;" value="" disabled>
+            
+            <input type="hidden" name="type" value="delete-item">
+            
+            <p class="uk-text-center uk-margin-medium-top">
+                <button class="uk-button uk-button-danger uk-modal-close" type="button">Отмена</button>
+                <button id="deleteItemID" class="uk-button uk-button-primary" type="submit" name="ID" value="">Удалить</button>
+            </p>
+        </form>
+    </div>
+</div>';
+
+
+
+
+
+
+
+
+
+// Модальное окно для редактирования товара
+echo'
+<div id="modal-example-8" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+        <form enctype="multipart/form-data" id="edititemform" method="post" action="../pages/edit.php">
+            <legend class="uk-legend">Редактирование категории</legend>
+
+            <div class="container uk-margin-medium-top">
+                <div class="row">
+                    <label><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Если не выбрать файл, то картинка останется прежней"></i> Картинка категории (будет сжата до квадратной):</label><br>
+                    <input class="uk-margin-small-top" type="file" id="file" name="file" />
+                </div>
+                <div class="row">
+                    <span class="uk-margin-small-top" id="output"></span>
+                </div>
+            </div>
+
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
+            <input id="editItemName" class="uk-input" name="newname" value="" required>
+            
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
+            <input id="editItemName" class="uk-input" name="newname" value="" required>
+            
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
+            <input id="editItemName" class="uk-input" name="newname" value="" required>
+
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
+            <input id="editItemName" class="uk-input" name="newname" value="" required>
+         
+            
+
+
+
+
+
+            <div class="container uk-margin-medium-top">
+                <div uk-form-custom="target: true">
+                    <label><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Если не выбрать файл, то картинка останется прежней"></i> Картинка категории (будет сжата до квадратной):</label><br>
+                    
+                    <input class="uk-margin-small-top" type="file" id="file2" name="file" />
+                    <input class="uk-input uk-form-width-medium" type="text" placeholder="Выбрать файл" disabled>
+                    
+                </div>
+                <div class="row">
+                    <span class="uk-margin-small-top" id="output2"></span>
+                </div>
+            </div>
+
+            
+
+
+
+
+            <input type="hidden" name="type" value="edit-category">
+            
+            <p class="uk-text-center uk-margin-medium-top">
+                <button class="uk-button uk-button-danger uk-modal-close" type="button">Отмена</button>
+                <button id="editItemOldCategory" class="uk-button uk-button-primary" type="submit" name="oldcategory" value="">Сохранить</button>
+            </p>
+        </form>
+    </div> 
+</div>';
+
+// Модальное окно для добавления новой товара
+echo'
+<div id="modal-example-9" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+        <form enctype="multipart/form-data" id="newitemform" method="post" action="../pages/new.php">
+            <legend class="uk-legend">Добавление новой категории</legend>
+
+            <div class="container uk-margin-medium-top">
+                <div class="row">
+                    <label><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Если не выбрать файл, то будет установлена картинка по-умолчанию"></i> Картинка категории (будет сжата до квадратной):</label><br>
+                    <input class="uk-margin-small-top" type="file" id="file" name="file" />
+                </div>
+                <div class="row">
+                    <span class="uk-margin-small-top" id="output"></span>
+                </div>
+            </div>
+
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Имя отображается на сайте, например \'Икра\'"></i> Имя категории:</p>
+            <input id="newItemName" class="uk-input" name="newname" value="" required>
+            
+            <p class="uk-margin-medium-top" style="margin-bottom: 10px;"><i class="fa fa-info-circle" style="color: black;" uk-tooltip="Категория отображается в адресной строке и должна быть написана на английском, например \'ikra\'"></i> Название категории:</p>
+            <input id="newItemCategory" class="uk-input" name="newcategory" value="" required>
+            
+            <input type="hidden" name="type" value="new-category">
+            
+            <p class="uk-text-center uk-margin-medium-top">
+                <button class="uk-button uk-button-danger uk-modal-close" type="button">Отмена</button>
+                <button id="newItemOldCategory" class="uk-button uk-button-primary" type="submit" name="oldcategory" value="">Сохранить</button>
+            </p>
+        </form>
+    </div> 
+</div>';
 ?>
---
+++ pages/test.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <label>Загрузить один файл:</label>
+            <input type="file" id="file" name="file" />
+        </div>
+        <div class="row">
+            <span id="output"></span>
+        </div>
+    </div>
+</body>
+</html>
+
+<script>
+function handleFileSelect(evt) {
+    var file = evt.target.files; // FileList object
+    var f = file[0];
+    // Only process image files.
+    if (!f.type.match('image.*')) {
+        alert("Image only please....");
+    }
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+        return function(e) {
+            // Render thumbnail.
+            var span = document.createElement('span');
+            span.innerHTML = ['<img class="thumb" title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+            document.getElementById('output').insertBefore(span, null);
+        };
+    })(f);
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+}
+document.getElementById('file').addEventListener('change', handleFileSelect, false);
+</script>
