6b3eaf9f7eb82e2d49c5e6c83431551586839909
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 22:18:18 +0700

    Добавляю заглушку, когда товара нет

--- pages/category-content.php
+++ pages/category-content.php
@@ -59,7 +59,7 @@
                 </a>';
             } elseif ((mysqli_num_rows($result) == 0)) {
                 echo'
-                <div>
+                <div style="width:100%">
                     <p style="font-size: 72pt; test-align: center;">Ой..</p>
                     <p style="font-size: 48pt; test-align: center;">Кажется, что в этой категории пока что нет товаров..</p>
                 </div>';
