c3e88d8cf7d9160cf8ad9b01179967807d8b0422
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 20:17:57 +0700

    Добавляю возможность редактирования товара

--- pages/edit.php
+++ pages/edit.php
@@ -67,12 +67,13 @@
 
             $tagstock = (isset($_POST['newtagstock']) ? 1 : 0);
             $tagnew = (isset($_POST['newtagnew']) ? 1 : 0);
-            $price = ($_POST['newprice']==0 ? NULL : $_POST['newprice']);
-            $count = ($_POST['newcount']==0 ? NULL : $_POST['newcount']);
+
+            if ($_POST['newprice']<>0) $price = ", `Price`='".$_POST['newprice']."'"; else $price = "";
+            if ($_POST['newcount']<>0) $count = ", `Count`='".$_POST['newcount']."'"; else $count = "";
 
             // Обновляем строку, если до этого не нашли таких же категорий
             if (mysqli_num_rows($result) <> 0) {
-                $sql = "UPDATE `production` SET `Image`='". $imgsrc ."', `Name`='". $_POST['newname'] ."', `Description`='". $_POST['newdescription'] ."', `Category`='". $_POST['newcategory'] ."', `Price`='". $price ."', `Count`='". $count ."', `Is-new`='". $tagnew ."', `Is-no-stock`='". $tagstock ."' WHERE `ID`='". $_POST['id'] ."'";
+                $sql = "UPDATE `production` SET `Image`='". $imgsrc ."', `Name`='". $_POST['newname'] ."', `Description`='". $_POST['newdescription'] ."', `Category`='". $_POST['newcategory'] ."'". $price . $count ."', `Is-new`='". $tagnew ."', `Is-no-stock`='". $tagstock ."' WHERE `ID`='". $_POST['id'] ."'";
                 echo $sql;
                 $result = mysqli_query($link, $sql);
 
