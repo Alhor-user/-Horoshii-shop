e7c0eef6b4434957c738354c4b31a64e1652ed76
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 14:10:23 +0700

    Добавляю возможность редактирования товара

--- pages/test.php
+++ pages/test.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../js/custom.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -18,27 +19,3 @@
     </div>
 </body>
 </html>
-
-<script>
-function handleFileSelect(evt) {
-    var file = evt.target.files; // FileList object
-    var f = file[0];
-    // Only process image files.
-    if (!f.type.match('image.*')) {
-        alert("Image only please....");
-    }
-    var reader = new FileReader();
-    // Closure to capture the file information.
-    reader.onload = (function(theFile) {
-        return function(e) {
-            // Render thumbnail.
-            var span = document.createElement('span');
-            span.innerHTML = ['<img class="thumb" title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
-            document.getElementById('output').insertBefore(span, null);
-        };
-    })(f);
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
-}
-document.getElementById('file').addEventListener('change', handleFileSelect, false);
-</script>
