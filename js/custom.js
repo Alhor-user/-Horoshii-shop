2adc329483f18550776f1306e8c90584b43a7810
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 12:49:53 +0700

    Перенёс скрипты и модальные окна в отдельные файлы

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+function DeleteCategory(category, name) {
+    console.log(category);
+    console.log(name);
+    document.getElementById('deleteategoryform').reset();
+    document.getElementById('deleteCategoryCategory').setAttribute('value', category);
+    document.getElementById('deleteCategoryName').setAttribute('value', name);
+}
+
+function EditCategory(category, name) {
+    document.getElementById('editcategoryform').reset();
+    document.getElementById('editCategoryCategory').setAttribute('value', category);
+    document.getElementById('editCategoryOldCategory').setAttribute('value', category);
+    document.getElementById('editCategoryName').setAttribute('value', name);
+}
+
+function DeleteItem(category, name) {
+    console.log(category);
+    console.log(name);
+    document.getElementById('deleteitemform').reset();
+    document.getElementById('deleteItemCategory').setAttribute('value', category);
+    document.getElementById('deleteItemName').setAttribute('value', name);
+}
+
+function EditItem(category, name) {
+    document.getElementById('edititemform').reset();
+    document.getElementById('editItemCategory').setAttribute('value', category);
+    document.getElementById('editItemOldCategory').setAttribute('value', category);
+    document.getElementById('editItemName').setAttribute('value', name);
+}
+
+function handleFileSelect(evt) {
+    var file = evt.target.files; // FileList object
+    var f = file[0];
+    // Only process image files.
+    if (!f.type.match('image.*')) {
+        alert("Image only please....");
+    }
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+        return function(e) {
+            // Render thumbnail.
+            var span = document.createElement('span');
+            span.innerHTML = ['<img class="thumb" title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+            document.getElementById('output').insertBefore(span, null);
+        };
+    })(f);
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+}
+document.getElementById('file').addEventListener('change', handleFileSelect, false);
--- pages/head.php
+++ pages/head.php
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" type="text/css">
 <script src="../js/uikit.min.js"></script>
 <script src="../js/uikit-icons.min.js"></script>
+<script src="../js/custom.js"></script>
 <!-- Yandex.Metrika counter --> 
 <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym"); ym(72422701, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/72422701" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
 <!-- /Yandex.Metrika counter -->
