ca93868a7ff1e0289df52e1e95f9541859eaf3a8
Aleksandr Horoshko <alecsandr-hor@yandex.ru>
2021-03-18 22:13:57 +0700

    Добавляю заглушку, когда товара нет

--- pages/category-content.php
+++ pages/category-content.php
@@ -14,10 +14,10 @@
     <div class="uk-width-1-1" style="margin: 0 10vw;">
         <div class="uk-grid-column-small uk-grid-row-small uk-text-center uk-flex-center uk-child-width-1-5@m" uk-grid style="margin: 0;">
 
-
             <?php
             while ($row = mysqli_fetch_array($result)) {
-                echo '<div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">';
+                echo '
+                <div class="uk-inline-clip uk-transition-toggle border-round" tabindex="0" style="padding: 0; cursor: pointer; margin-left: 15px;">';
                 
                 if (hash('md5', $_SESSION["login"]) == $_SESSION["key"]) {
                     echo '
@@ -49,13 +49,14 @@
             }; 
             
             if (hash('md5', $_SESSION["login"]) == $_SESSION["key"]) {
-                echo '<a href="#modal-example-9" onclick="NewItem(\'', $category,'\')" uk-toggle>
-                        <div class="uk-card uk-card-default" style="box-shadow: none;">
-                            <div class="uk-card-media-top">
-                                <img src="../img/new-item.png" alt="" class="category-card" style="height: auto;">
-                            </div>
+                echo '
+                <a href="#modal-example-9" onclick="NewItem(\'', $category,'\')" uk-toggle>
+                    <div class="uk-card uk-card-default" style="box-shadow: none;">
+                        <div class="uk-card-media-top">
+                            <img src="../img/new-item.png" alt="" class="category-card" style="height: auto;">
                         </div>
-                    </a>';
+                    </div>
+                </a>';
             } elseif ((mysqli_num_rows($result) == 0)) {
                 echo'
                 <div>
--- pages/footer.php
+++ pages/footer.php
@@ -1,5 +1,5 @@
 <footer class="uk-section uk-section-secondary uk-section-xsmall" style="background-image: url(../img/header-wave2.jpg); background-size: 100%;">
-  <div uk-grid class="uk-width-1-1 uk-grid-collapse uk-child-width-expand">
+    <div uk-grid class="uk-width-1-1 uk-grid-collapse uk-child-width-expand">
         <div class="uk-width-1-4">
             <a href="../"><img src="../img/logo.png" alt="Logo" class="img-logo" style="margin: 0 0 0 25px;"></a>
         </div>
@@ -10,7 +10,7 @@
         </div>
         <div class="uk-width-1-4 uk-height-1-1">
 
-        <?php // Подгружаем из БД записи, что должны быть в футере и хедере
+            <?php // Подгружаем из БД записи, что должны быть в футере и хедере
                 echo '<p class="header-footer-work-text size-1" style="margin: 0 25px 5px 0;">';
                 $sql = "SELECT `Content` FROM `content` WHERE `ID`='1'";
                 $result = mysqli_query($link, $sql);
@@ -40,8 +40,8 @@
     </div>
     <hr>
     <div class="uk-flex uk-flex-between">
-    <div><i style="margin-left:25px; text-align: left; color: #F5F4F5; font-size: .75rem; font-weight: 400;">Copyright © 2010 - 2021 "Магазин Хороший"</i></div>
-    <div><i style="margin-right: 25px; text-align: right; color: #F5F4F5; font-size: .75rem; font-weight: 400;">Разработчик: alecsandr-hor@yandex.ru</i></div>
+        <div><i style="margin-left:25px; text-align: left; color: #F5F4F5; font-size: .75rem; font-weight: 400;">Copyright © 2010 - 2021 "Магазин Хороший"</i></div>
+        <div><i style="margin-right: 25px; text-align: right; color: #F5F4F5; font-size: .75rem; font-weight: 400;">Разработчик: alecsandr-hor@yandex.ru</i></div>
     </div>
 </footer>
 
